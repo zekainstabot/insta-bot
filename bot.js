083b624e2d56e27f7b00e2b91058b03665179f48
zekainstabot <1999farpi@gmail.com>
2026-09-16 23:02:45 +0330

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,64 @@
+const { Telegraf } = require('telegraf');
+const { HttpsProxyAgent } = require('https-proxy-agent');
+const { execFile } = require('child_process');
+const fs = require('fs');
+const path = require('path');
+
+const agent = new HttpsProxyAgent('http://127.0.0.1:12334');
+
+const bot = new Telegraf('8745804140:AAG-Uy9GKFuO0E2CtE1yoEqDFp78LfB1KGA', {
+  telegram: {
+    agent: agent
+  }
+});
+
+bot.start((ctx) => {
+  ctx.reply('سلام 👋 لینک اینستاگرامت رو بفرست.');
+});
+
+bot.on('text', async (ctx) => {
+  const text = ctx.message.text;
+
+  if (!text.includes('instagram.com')) {
+    return ctx.reply('لطفاً لینک اینستاگرام بفرست.');
+  }
+
+ const fileName = 'instagram_' + Date.now() + '.mp4';
+  const filePath = path.join(__dirname, fileName);
+
+  try {
+    await ctx.reply('در حال دانلود... ⏳');
+
+    execFile(
+      path.join(__dirname, 'yt-dlp.exe'),
+      [
+        '--no-playlist',
+        '-f', 'best[ext=mp4]/best',
+        '-o', filePath,
+        text
+      ],
+      async (error, stdout, stderr) => {
+        if (error) {
+          console.log(error);
+          return ctx.reply('دانلود انجام نشد ❌');
+        }
+
+        if (!fs.existsSync(filePath)) {
+          return ctx.reply('فایل پیدا نشد ❌');
+        }
+
+        await ctx.replyWithVideo({ source: filePath });
+
+        fs.unlinkSync(filePath);
+      }
+    );
+
+  } catch (error) {
+    console.log(error);
+    ctx.reply('یک خطا رخ داد ❌');
+  }
+});
+
+bot.launch();
+
+console.log('ربات روشن شد ✅');
