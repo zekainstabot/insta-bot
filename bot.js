168befc4f4ab27e365c30cbdd9ad6a7f2ccbd559
zekainstabot <1999farpi@gmail.com>
2026-09-18 11:33:03 +0330

    Update bot.js

--- bot.js
+++ bot.js
@@ -597,7 +597,7 @@ bot.on('text', async (ctx) => {
       return;
     }
 
- const fileName = instagram_${Date.now()}.mp4;
+ const fileName = `instagram_${Date.now()}.mp4`;
     const filePath =
       path.join('/tmp', fileName);
 
