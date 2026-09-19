73296d5c708fe3f45c1243c0b52195ba8b1fefe3
zekainstabot <1999farpi@gmail.com>
2026-09-19 08:20:50 +0330

    Update downloader.js

--- services/downloader.js
+++ services/downloader.js
@@ -1,1 +1,12 @@
+const ytDlp = require('yt-dlp-exec');
 
+async function downloadInstagram(url, outputPath) {
+  await ytDlp(url, {
+    output: outputPath,
+    format: 'mp4/best',
+  });
+
+  return outputPath;
+}
+
+module.exports = { downloadInstagram };
