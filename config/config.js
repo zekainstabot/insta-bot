41486f561494e142e3ce5a51f9b4f86b8a07c8b6
zekainstabot <1999farpi@gmail.com>
2026-09-19 13:37:56 +0330

    Create config.js

---
+++ config/config.js
@@ -0,0 +1,9 @@
+const BOT_TOKEN = process.env.BOT_TOKEN;
+const DATABASE_URL = process.env.DATABASE_URL;
+const ADMIN_ID = process.env.ADMIN_ID;
+
+module.exports = {
+  BOT_TOKEN,
+  DATABASE_URL,
+  ADMIN_ID
+};
